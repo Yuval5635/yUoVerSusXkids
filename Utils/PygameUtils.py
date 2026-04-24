4b03667dcdd7e13038fb6baa8e187b74ae7a5745
Yuval5635 <yuyudiamant@gmail.com>
2026-04-24 14:53:08 +0300

    added mouse basic functions to PygameUtils.py

--- Utils/PygameUtils.py
+++ Utils/PygameUtils.py
@@ -1,15 +1,22 @@
 import pygame as pg
+from Utils.Vector import Vector
 
 class PygameUtils:
     keys = None
     old_keys = None
     events = []
+    mouse_position = None
+    mouse_buttons = None
+    mouse_old_buttons = None
 
     @staticmethod
     def update():
         PygameUtils.old_keys = PygameUtils.keys
         PygameUtils.keys = pg.key.get_pressed()
         PygameUtils.events = pg.event.get()
+        PygameUtils.mouse_position = Vector(pg.mouse.get_pos()[0], pg.mouse.get_pos()[1])
+        PygameUtils.mouse_old_buttons_buttons = PygameUtils.mouse_buttons
+        PygameUtils.mouse_buttons = pg.mouse.get_pressed()
 
     @staticmethod
     def is_key_held(key):
@@ -18,3 +25,15 @@ class PygameUtils:
     @staticmethod
     def is_key_pressed(key):
         return PygameUtils.keys[key] and not PygameUtils.old_keys[key]
+
+    @staticmethod
+    def get_mouse_position():
+        return PygameUtils.mouse_position
+
+    @staticmethod
+    def is_mouse_held(button):
+        return PygameUtils.mouse_buttons[button]
+
+    @staticmethod
+    def is_mouse_pressed(button):
+        return PygameUtils.mouse_buttons[button] and not PygameUtils.mouse_old_buttons[button]
